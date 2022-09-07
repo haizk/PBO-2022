02caef6ee8a548ba672f6c5ba2469f7599000430
haizk <haizkb@gmail.com>
2022-09-07 21:22:42 +0700

    LatihanLong

--- Praktikum/Praktikum 2/2.1 Variabel/LatihanLong.java
+++ Praktikum/Praktikum 2/2.1 Variabel/LatihanLong.java
@@ -1,5 +1,15 @@
 public class LatihanLong {
     public static void main(String[] args) {
-        System.out.println("Halo Haizk!");
+        // long p = 2147483648; <- terjadi error
+        long p = 2147483648L; // <- perbaikan
+        System.out.println(p);
     }
 }
+
+/* Apa maksud dari pesan error yang muncul?
+ ** "The literal 2147483648 of type int is out of range"
+ ** Angka 2147483648 dianggap sebagai tipe data int dan melebihi range dari int sehingga muncul error tersebut.
+ *
+ * Bagaimana memperbaiki source code tersebut?
+ ** Dengan cara menambahkan L pada akhir angka 2147483648 sehingga bisa dianggap sebagai tipe data long dan tidak menimbulkan error.
+ */
