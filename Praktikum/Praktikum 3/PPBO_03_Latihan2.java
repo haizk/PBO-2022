a6792ed7cf8efea0439e82f415551c41822bb253
haizk <haizkb@gmail.com>
2022-09-18 21:39:08 +0700

    ppbo 3 lat 2

--- Praktikum/Praktikum 3/PPBO_03_Latihan2.java
+++ Praktikum/Praktikum 3/PPBO_03_Latihan2.java
@@ -3,13 +3,33 @@
 
 import java.util.Scanner;
 
-class PPBO_03_Latihan2 {
+public class PPBO_03_Latihan2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int year = sc.nextInt();
         
         // tuliskan kodemu di sini
-            
+        /*
+            leap year sepengetahuan saya:
+            - apabila kelipatan 100, harus habis dibagi 400
+            - habis dibagi 4
+            - untuk masa Anno Domini (AD) dimulai pada tahun ke-8
+
+            berikut adalah pengecek leap year untuk masa Anno Domini
+        */
+
+        /* MORE READABLE CODE
+        if(year % 100 == 0 && year % 400 == 0 && year >= 8)
+            System.out.println(year + " is a leap year");
+        else if(year % 100 == 0 && year >= 8)
+            System.out.println(year + " is not a leap year");
+        else if(year % 4 == 0 && year >= 8)
+            System.out.println(year + " is a leap year");
+        else
+            System.out.println(year + " is not a leap year");
+        */
+
+        System.out.println(year + ((year % 100 == 0 && year % 400 == 0 && year >= 8) ? " is a leap year" : ((year % 100 == 0 && year >= 8) ? " is not a leap year" : ((year % 4 == 0 && year >= 8) ? " is a leap year" : " is not a leap year"))));
 
         sc.close();
     }
