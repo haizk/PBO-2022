8f31f878c3c45032314c4859cebcd53687ff666b
haizk <haizkb@gmail.com>
2022-09-19 00:21:15 +0700

    praktikum-3 do while demo

---
+++ Praktikum/Praktikum 3/DoWhileDemo.java
@@ -0,0 +1,14 @@
+public class DoWhileDemo {
+    public static void main(String[] args) {
+        int arr[] = {5, 1, 4, 2, 3};
+        // for (int i = 1; i < 6; i++) {
+        //     System.out.println("elemen ke-" + i + " = " + arr[i]);
+        // }
+
+        // PERBAIKAN
+        int i = 0;
+        do {
+            System.out.println("elemen ke-" + (i+1) + " = " + arr[i]);
+        } while (++i < 5);
+    }
+}
