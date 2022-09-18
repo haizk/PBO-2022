4b279486740aaf9ee641694e150a1a1bbcd459e6
haizk <haizkb@gmail.com>
2022-09-19 01:01:36 +0700

    minggu 4 lat 2

---
+++ Teori/Minggu 4 (Latihan)/Latihan2A.java
@@ -0,0 +1,18 @@
+public class Latihan2A {
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int count = 0; count < 10; count++)
+            sum += count;
+        System.out.println(sum);
+    }
+}
+
+/*
+    2.A Rewrite the following while loops as for statements
+    
+    int count = 0, sum = 0;
+    while(count < 10) {
+        sum += count;
+        count ++;
+    }
+*/
---
+++ Teori/Minggu 4 (Latihan)/Latihan2B.java
@@ -0,0 +1,18 @@
+public class Latihan2B {
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int count = 1; count <= 30; count+=3)
+            sum += count;
+        System.out.println(sum);
+    }
+}
+
+/*
+    2.A Rewrite the following while loops as for statements
+    
+    int count = 1, sum = 0;
+    while(count <= 30) {
+        sum += count;
+        count += 3;
+    }
+*/
