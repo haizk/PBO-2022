9615449e09b633b768014f75167c2249914f86c6
haizk <haizkb@gmail.com>
2022-09-18 22:19:13 +0700

    ppbo 3 lat 4

--- Praktikum/Praktikum 3/PPBO_03_Latihan4.java
+++ Praktikum/Praktikum 3/PPBO_03_Latihan4.java
@@ -4,9 +4,15 @@
 public class PPBO_03_Latihan4 {
     public static void main(String[] args) {
         int arr[] = {5, 1, 4, 2, 3};
-        for (int i = 1; i < 6; i++) {
+        // for (int i = 1; i < 6; i++) {
+        //     System.out.println("elemen ke-" + i + " = " + arr[i]);
+        // }
+
+        // PERBAIKAN
+        int i = 0;
+        do {
             System.out.println("elemen ke-" + i + " = " + arr[i]);
-        }
+        } while (++i < 5);
     }
 }
 
